c3666622e9fe767b93966aa8aab5e2fc04b3417e
Ajit kumar <kajit0408@gmail.com>
2023-07-08 11:15:05 +0530

    Initialised react App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <span>
+        <h1>Chalo Shuruat Kre</h1>
+      </span>
+    </div>
+  );
+}
+
+export default App;
