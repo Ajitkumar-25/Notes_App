b1cb5db4d6ac0d417b8625058925b93acd4d8561
Ajitkumar-25 <kajit0408@gmail.com>
2024-05-30 00:38:08 +0530

    updated atlas url

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURL = "mongodb://0.0.0.0:27017/Notes_App";
+// const mongoURL = "mongodb://0.0.0.0:27017/Notes_App";
+const mongoURL = "mongodb+srv://kajit0408:dFzDIRdPnpJ4wPE7@cluster1.psvnoka.mongodb.net/NotesApp?retryWrites=true&w=majority&appName=Cluster1";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURL, {
