9c8c2ac9984aab8725be20ce114bf6019104c3ab
Ajitkumar-25 <kajit0408@gmail.com>
2024-05-31 19:24:23 +0530

    added frontend origin to cors

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 //available routes
